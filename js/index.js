7b3a1eca99b0473aae737654700c65c356941515
Ricardo <ricardo@sistemasdinamica.com.br>
2019-07-29 20:36:11 -0300

    calculo da media ponderada

---
+++ js/index.js
@@ -0,0 +1,13 @@
+
+var nota1 = parseInt(prompt("Digite a nota 1: "));
+var nota2 = parseInt(prompt("Digite a nota 2: "));
+var nota3 = parseInt(prompt("Digite a nota 3: "));
+
+var mediaponderada = (nota1 * 2 + nota2 * 3 + nota3 * 5) / 10;
+
+
+
+document.write("A média ponderada é: " +mediaponderada);
+
+
+
